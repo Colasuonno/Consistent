efc84977398dc7d0997c2880a0728ca917d1d43b
Colasuonno <colasuonno.scuola@gmail.com>
2020-06-24 12:52:17 +0200

    Addede sentence filter

--- constistent.py
+++ constistent.py
@@ -1,6 +1,6 @@
 import requests
 
-query = "Dove è nato Dante Alighieri"
+query = "Cos'è l'entropia"
 url = "https://api.qwant.com/api/search/web?count=10&offset=0&q={key}&t=web&uiv=4&locale=it_It"
 #results = qwant.items("site:wikipedia.org " + query, count=5)
 
@@ -32,13 +32,41 @@ def consistent(item, query):
     Once we have the result, the desc is parsed with the correct phrase
     
     """
-    
+
+def closest_space_in_sentence(sentence, query_words):
+    ws = sentence.split(" ")
+    min_space = 100000 # Just high number
+
+    for i in range(len(ws)):
+        for j in range(len(ws)-1, -1, -1):
+            if str(ws[i]).lower() in query_words and str(ws[j]).lower() in query_words:
+                pos = abs( i - j )
+                if pos < min_space:
+                    min_space = pos
+    return min_space
+  #  print("MIN SPACE FOR " + sentence)
+  #  print("QUERY: " + str(query_words))
+  #  print("MIN: " + str(min_space))
+            
+def analyze(query_words, valid, alternatives):
+    current_min_space = 10000 # high number
+    current_min_sentence = -1
+    for s in valid.split("."):
+        spaces = closest_space_in_sentence(s, query_words)
+        if spaces < current_min_space:
+            current_min_space = spaces
+            current_min_sentence = s
+    print("SPACES MIN: " + str(current_min_space))
+    print("RESULT:")
+    print(current_min_sentence)
+
 
 def answer(query):
     results = requests.get(str(url).replace("{key}", query).replace(" ", "%20"),headers={'User-agent': 'Mozilla/5.0'})
     r = results.json()
     current_valid_points = -1
     current_valid = None
+    alternatives = []
     items = r["data"]["result"]["items"]
     for i in items:
         flag = False
@@ -49,16 +77,25 @@ def answer(query):
         if not flag:
             print("ANALYZE " + i["url"])
       #      print(i["desc"])
-            points = contains_all(query, i["desc"])
+            points, cws = contains_all(query, i["desc"])
             if points <= MIN_POINTS:
+                if current_valid_points != -1 and current_valid_points == points:
+                    alternatives.append(i)
                 if current_valid_points == -1 or current_valid_points > points:
                     current_valid_points = points
                     current_valid = i
+                
     if current_valid_points != -1:
         print("VALID POINTS " + str(current_valid_points))
         print(current_valid["desc"])
+    for a in alternatives:
+        print("\n")
+        print("ALTERNATIVE SAME POINTS")
+        print(a["desc"])
+        print("\n")
                 
-    print("\n")
+    return cws, current_valid, alternatives
+
 
 
 def contains_all(query, desc):
@@ -79,6 +116,7 @@ def contains_all(query, desc):
       #          print(wq)
                 if wq in ws:
                     ws.remove(wq)
-    return len(ws)
+    return len(ws), cws
             
-answer(query)
+cws, current_valid, alternatives = answer(query)
+analyze(cws, current_valid["desc"], alternatives)
